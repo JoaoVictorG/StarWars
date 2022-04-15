76006ff896d34900957e63981d015846ecd56880
JoaoVictorG <joaovictorgon36@gmail.com>
2022-04-15 20:47:57 -0300

    divisor de campos

--- star-wars/src/pages/components/card/index.js
+++ star-wars/src/pages/components/card/index.js
@@ -3,82 +3,31 @@ import { useEffect, useState } from "react";
 
 const Card = () => {
   const [list, setList] = useState([]);
-  const [typeSearch, setTypeSearch] = useState(0);
 
-  const getApi = (search) => {
-    axios.get(`https://swapi.dev/api/${search}`).then((result) => {
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/starships").then((result) => {
       setList(result.data.results);
       console.log(result.data.results);
     });
-  };
-
-  //   const Foo = ({ typeSearch }) => {
-  //     switch (typeSearch) {
-  //       case 0:
-  //         return (
-  //           <div>
-  //             <p>valor 1</p>
-  //           </div>
-  //         );
-  //         break;
-  //       case 1:
-  //         return (
-  //           <div>
-  //             <p>valor 2</p>
-  //           </div>
-  //         );
-  //         break;
-  //       case 3:
-  //         return (
-  //           <div>
-  //             <p>valor 3</p>
-  //           </div>
-  //         );
-  //       default:
-  //         return null;
-  //     }
-  //   };
-
-  // useEffect(() => {
-  // },[])
+  }, []);
 
   return (
     <>
       <div>
-        <button
-          value="planets"
-          onClick={(e) => {
-            getApi(e.target.value);
-            setTypeSearch(0);
-          }}
-        >
-          Planetas
-        </button>
-        <button
-          value="people"
-          onClick={(e) => {
-            getApi(e.target.value);
-            setTypeSearch(1);
-          }}
-        >
-          Pessoas
-        </button>
-        <button
-          value="starships"
-          onClick={(e) => {
-            getApi(e.target.value);
-            setTypeSearch(2);
-          }}
-        >
-          Naves
-        </button>
-      </div>
-      <div>
         {list.map((item) => {
           return (
-            <div>
-              <p>{item.name}</p>
-              <p>{item.climate}</p>
+            <div key={item.url}>
+              {/* <p>Numero: {item.url}</p> */}
+              <p>Nome: {item.name}</p>
+              <p>Modelo: {item.model}</p>
+              <p>Fabricante: {item.manufacturer}</p>
+              <p>Custo: {item.cost_in_credits}</p>
+              <p>Comprimento: {item.length}</p>
+              <p>{item.max_atmosphering_speed}</p>
+              <p>{item.crew}</p>
+              <p>{item.passengers}</p>
+              <p>{item.cargo_capacity}</p>
+              <hr />
             </div>
           );
         })}
